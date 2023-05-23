4e45db33a52543931686bba6d059ed22f5dfee58
Doxy <doxyai.yt@pm.me>
2023-05-22 22:42:40 -0700

    Added Requests as a Requirement
    I completely forgot that this package isn't part of the python standard library!

--- zatsu.py
+++ zatsu.py
@@ -3,7 +3,7 @@
 This script launches a singleton instance of ZatsuDachi.
 
 Usage: python zatsu.py
-Setup: pip install colour flask flask_socketio tk
+Setup: pip install colour flask flask_socketio tk requests
 
 The _singletonApp module is responsible for ensuring that only one instance of the application is running.
 The script runs an infinite loop to keep the main thread alive.
